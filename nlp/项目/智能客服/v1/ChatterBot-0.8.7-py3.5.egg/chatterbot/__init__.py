682deed4b34f9cea1f8ce1be1b6207ddad276a19
Bin Zhou <binzhouchn@gmail.com>
2018-06-12 21:16:05 +0800

    nlp and kg learning 20180612

--- nlp/项目/智能客服/v1/ChatterBot-0.8.7-py3.5.egg/chatterbot/__init__.py
+++ nlp/项目/智能客服/v1/ChatterBot-0.8.7-py3.5.egg/chatterbot/__init__.py
@@ -11,7 +11,7 @@ __version__ = '0.8.7'
 __orig_author__ = 'Gunther Cox'
 __orig_email__ = 'gunthercx@gmail.com'
 __orig_author_url__ = 'https://github.com/gunthercox/ChatterBot'
-__edit_author_url__ = 'https://github.com/binzhouchn'
+__edit_author_url__ = 'https://github.com/binzhouchn/ai_notes'
 
 __all__ = (
     'ChatBot',
--- nlp/项目/智能客服/v1/README.md
+++ nlp/项目/智能客服/v1/README.md
@@ -2,7 +2,7 @@
 
 2. 把/opt/anaconda3/lib/python3.5/site-packages/ChatterBot-0.8.7-py3.5.egg下面的chatterbot文件夹替换掉
 
-3. __main__.py 这个文件可以放在任何地方，flask入口从这里进
+3. 这个__main__.py文件可以放在任何地方，flask入口从这里进
 
 4. 运行__main__.py，然后可以用postman工具发送post请求发送报文
 
